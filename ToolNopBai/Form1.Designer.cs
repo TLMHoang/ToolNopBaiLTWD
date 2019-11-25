b8c402fa3b7765123c305c3d485664b9270dca75
Minh Hoàng <51706032+TLMHoang@users.noreply.github.com>
2019-11-25 20:26:00 +0700

    Tool

---
+++ ToolNopBai/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ToolNopBai
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.AutoSize = true;
+            this.btnAuto.Location = new System.Drawing.Point(206, 50);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(91, 23);
+            this.btnAuto.TabIndex = 0;
+            this.btnAuto.Text = "Auto";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnDienThongTin_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Lần  01",
+            "Lần  02",
+            "Lần  03",
+            "Lần  04",
+            "Lần  05",
+            "Lần  06",
+            "Lần  07",
+            "Lần  08",
+            "Lần  09",
+            "Lần  10"});
+            this.comboBox1.Location = new System.Drawing.Point(43, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(94, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lần";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(352, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(303, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 88);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnAuto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Tag = "";
+            this.Text = "Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ToolNopBai/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KAutoHelper;
+
+namespace ToolNopBai
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public static void MoApp()
+        {
+            Process p = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "CMD.exe";
+            startInfo.Arguments = "/C Start HuTechPractice.exe";
+
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            p.StartInfo = startInfo;
+            p.Start();
+        }
+
+        private void btnDienThongTin_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || comboBox1.Text == "")
+            {
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Nhập lần nộp");
+                }
+                else
+                {
+                    MessageBox.Show("Chọn dường dẫn file");
+                }
+            }
+            else
+            {
+                IntPtr HWindow = IntPtr.Zero;
+
+                do
+                {
+                    Process[] p = Process.GetProcessesByName("HuTechPractice");
+                    switch (p.Length)
+                    {
+                        case 0:
+                            MoApp();
+                            Thread.Sleep(1000);
+                            break;
+                        case 1:
+                            HWindow = p[0].MainWindowHandle;
+                            break;
+                        default:
+                            for (int i = 1; i < p.Length; i++)
+                            {
+                                p[i].Kill();
+                                Thread.Sleep(10);
+                            }
+                            break;
+                    }
+                } while (HWindow == IntPtr.Zero);
+
+                IntPtr ChildH = IntPtr.Zero;
+                ChildH = AutoControl.FindHandle(HWindow, "WindowsForms10.COMBOBOX.app.0.141b42a_r6_ad1", null);
+
+                AutoControl.SendText(ChildH, comboBox1.Text);
+                List<IntPtr> AChild = AutoControl.FindHandles(HWindow, "WindowsForms10.EDIT.app.0.141b42a_r6_ad1", null);
+
+                
+                AutoControl.SendText(AChild[0], textBox1.Text);
+                AutoControl.SendText(AChild[1], "Lập Trình Window");
+                AutoControl.SendText(AChild[2], "17DTHB2");
+                AutoControl.SendText(AChild[3], "Trần Lê Minh Hoàng");
+                AutoControl.SendText(AChild[4], "fsminhhoangfs@gmail.com");
+
+                IntPtr ChildB = AutoControl.FindHandle(HWindow, "WindowsForms10.BUTTON.app.0.141b42a_r6_ad1", "Nộp bài tập");
+
+                AutoControl.SendClickOnControlByHandle(ChildB);
+            }
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = open.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process[] p = Process.GetProcessesByName("HuTechPractice");
+
+            if (p.Length == 0)
+            {
+                this.Close();
+            }
+            else
+            {
+                p[0].Kill();
+                this.Close();
+            }
+        }
+    }
+}
